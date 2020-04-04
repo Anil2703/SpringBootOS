09deb18f243824c48c69c877813e4b132ddebe7a
Anil2703 <anilreddy34@gmail.com>
2020-04-05 01:03:27 +0530

    first commit

---
+++ src/main/java/com/rocky/SBOS.java
@@ -0,0 +1,12 @@
+package com.rocky;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBOS {
+    public static void main(String[] args) {
+        SpringApplication.run(SBOS.class,args);
+    }
+}
